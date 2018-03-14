fd12938177b6abf2dd4fb0c791e601b13925ebe6
Blake Kruppa <btkruppa513@gmail.com>
2018-03-14 10:01:04 -0400

    updated microservices examples

--- microservices/gateway-service/src/main/java/com/revature/Application.java
+++ microservices/gateway-service/src/main/java/com/revature/Application.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.filters.PreFilter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 @RestController
 public class Application {
